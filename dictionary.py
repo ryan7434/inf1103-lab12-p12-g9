04932d193482dba1b2c211ec7e9f0065c073b458
tan zekai <2601818@sit.singaporetech.edu.sg>
2026-09-20 17:13:41 +0800

    adding in the first 2 choices

---
+++ dictionary.py
@@ -0,0 +1,51 @@
+DIETARY_CHOICES = {
+    "1": {
+        "key": "halal",
+        "display_name": "Halal Preference (no pork etc)",
+        "restricted_terms": {
+            "pork",
+            "pork belly",
+            "minced pork",
+            "bacon",
+            "ham",
+            "lard",
+            "alcohol",
+            "beer",
+            "wine",
+            "cooking wine",
+            "rum",
+            "brandy",
+            "vodka",
+            "whisky"
+        }
+    },
+
+    "2": {
+        "key": "vegetarian",
+        "display_name": "Vegetarian (eggs & dairy OK)",
+        "restricted_terms": {
+            "chicken",
+            "chicken breast",
+            "chicken thigh",
+            "beef",
+            "pork",
+            "pork belly",
+            "minced pork",
+            "lamb",
+            "mutton",
+            "duck",
+            "turkey",
+            "fish",
+            "salmon",
+            "tuna",
+            "cod",
+            "prawn",
+            "shrimp",
+            "crab",
+            "squid",
+            "clam",
+            "mussel",
+            "oyster",
+            "seafood"
+        }
+    },
